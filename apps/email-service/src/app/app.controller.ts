1bf7cad58a02e08140e803aace1fde728030b23b
zeeshooo <zeeshan@invyce.com>
2021-09-28 11:41:18 +0500

    adding attachments workflow

--- apps/email-service/src/app/app.controller.ts
+++ apps/email-service/src/app/app.controller.ts
@@ -2,10 +2,20 @@ import { Controller, Get, Logger } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 import { AppService } from './app.service';
-import { SEND_FORGOT_PASSWORD, SEND_INVITATION } from '@invyce/send-email';
+import {
+  SEND_OTP,
+  SEND_FORGOT_PASSWORD,
+  SEND_INVITATION,
+} from '@invyce/send-email';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async GetData() {
+    return await this.appService.getData();
+  }
 
   @MessagePattern(SEND_INVITATION)
   async sendInvitation(@Payload() data: any) {
@@ -21,5 +31,12 @@ export class AppController {
     Logger.log(data);
 
     await this.appService.SendForgotPassword(data);
+  }
+  @MessagePattern(SEND_OTP)
+  async sendOtp(@Payload() data: any) {
+    Logger.log('Data is received.');
+    Logger.log(data);
+
+    await this.appService.SendOtp(data);
   }
 }
--- apps/email-service/src/app/app.service.ts
+++ apps/email-service/src/app/app.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import * as postmark from 'postmark';
-const client = new postmark.ServerClient(
-  'c2afabc2-c521-4a0f-9957-dbba1be9f943'
-);
+const client = new postmark.ServerClient(process.env.POSTMARK_TOKEN);
 
 @Injectable()
 export class AppService {
   constructor() {}
+
+  getData(): { message: string } {
+    return { message: 'Welcome to contacts!' };
+  }
 
   async SendInvitation(data) {
     const email = await client.sendEmailWithTemplate({
@@ -19,6 +21,15 @@ export class AppService {
   }
 
   async SendForgotPassword(data) {
+    const email = await client.sendEmailWithTemplate({
+      From: data.from,
+      To: data.to,
+      TemplateAlias: data.TemplateAlias,
+      TemplateModel: data.TemplateModel,
+    });
+    console.log(email, 'email successfully.');
+  }
+  async SendOtp(data) {
     const email = await client.sendEmailWithTemplate({
       From: data.from,
       To: data.to,
