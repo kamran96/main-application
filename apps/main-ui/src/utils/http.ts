29bd7f7a66061fcb809f8d157690f415d93d6d52
Kamran Jan <a@as-MacBook-Pro.local>
2021-11-17 15:59:45 +0500

    merged front end and backend in one branch

--- apps/main-ui/src/utils/http.ts
+++ apps/main-ui/src/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { DecriptionData } from './encription';
 
-const localIP = `http://192.168.6.235/`;
+const localIP = `http://192.168.1.22/`;
 let RailsBaseURL = '';
 let NodeBaseURL = ``;
 const cancelSource = axios.CancelToken.source();
