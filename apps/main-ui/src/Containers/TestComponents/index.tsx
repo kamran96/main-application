661a6de6e583aada8000aedbf53b7e56c06ec506
Kamran Jan <a@as-MacBook-Pro.local>
2021-11-22 17:09:42 +0500

    created microservice for csv upload

--- apps/main-ui/src/Containers/TestComponents/index.tsx
+++ apps/main-ui/src/Containers/TestComponents/index.tsx
@@ -1,7 +1,19 @@
 import { invycePersist } from '@invyce/invyce-persist';
 import { FC, useEffect } from 'react';
+import { useMutation } from 'react-query';
+import { CsvImportAPi } from '../../api';
 import { InvoiceImportManager } from '../../modules/Invoice/InvoiceImportManager';
 
 export const TestComponents: FC = () => {
-  return <InvoiceImportManager />;
+  const [mutateCsv, resMutateCsv] = useMutation(CsvImportAPi);
+
+  const handleLoad = async (payload) => {
+    await mutateCsv(payload, {
+      onSuccess: (data) => {
+        console.log(data);
+      },
+    });
+  };
+
+  return <InvoiceImportManager onLoad={handleLoad} />;
 };
