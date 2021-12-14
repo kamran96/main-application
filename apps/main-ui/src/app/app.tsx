e8aa6c600c6f4be784a727972aa0aa601bb818cd
System Administrator <root@as-MacBook-Pro.local>
2021-12-14 16:07:02 +0500

    more quick fixes

--- apps/main-ui/src/app/app.tsx
+++ apps/main-ui/src/app/app.tsx
@@ -1,17 +1,16 @@
-import React, { FC } from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import AppContainer from '../Containers/App/AppContainer';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+
+import { FC } from 'react';
 import { ReactQueryConfigProvider } from 'react-query';
+
+import AppContainer from '../Containers/App/AppContainer';
+import { GlobalManager } from '../hooks/globalContext/globalManager';
+import { GlobalStylesWrapper } from './globalStyles';
 
 /* Ant design css */
 // import "antd/dist/antd.css";
-import { GlobalManager } from '../hooks/globalContext/globalManager';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { sentryInit } from '../utils/integrations';
-import { GlobalStylesWrapper } from './globalStyles';
-
 const rqConfig = {
   queries: {
     staleTime: 1000 * 2 * 60, // that's one min.
