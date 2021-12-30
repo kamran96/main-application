cef54dbf46d35712e5069678a5c1fdffaf8ab97c
System Administrator <root@as-MacBook-Pro.local>
2021-12-30 12:07:40 +0500

    inv 389

--- apps/main-ui/src/Layout/DefaultLayout.tsx
+++ apps/main-ui/src/Layout/DefaultLayout.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { RouteConfigComponentProps, renderRoutes } from "react-router-config";
-import { Redirect } from "react-router";
-import { useGlobalContext } from "../hooks/globalContext/globalContext";
+import React, { useEffect } from 'react';
+import { RouteConfigComponentProps, renderRoutes } from 'react-router-config';
+import { Redirect } from 'react-router';
+import { useGlobalContext } from '../hooks/globalContext/globalContext';
+import { useHistory } from 'react-router-dom';
 
 export const DefaultLayout = (props: RouteConfigComponentProps) => {
   const { isUserLogin, userDetails } = useGlobalContext();
 
+  const history = useHistory();
+
+  console.log(history, 'history');
+
   if (isUserLogin) {
     if (userDetails && userDetails.organizationId && userDetails.branchId) {
-      return <Redirect to="/app/dashboard" />;
+      const state: any = history.location.state;
+      const route = state?.pathname || '/app/dashboard';
+      return <Redirect to={route} />;
     } else {
       return <Redirect to="/app/organizations" />;
     }
