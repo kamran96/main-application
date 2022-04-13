1893afe5e314266dac9e856883589991379fb575
zeeshooo <zeeshan@invyce.com>
2022-04-13 16:16:57 +0500

    chaagnes

--- apps/accounting-service/ormconfig.js
+++ apps/accounting-service/ormconfig.js
@@ -16,5 +16,5 @@ module.exports = {
     migrationsDir: './apps/accounting-service/migrations',
   },
   ssl:
-    process.env.NODE_ENV === 'development' ? {} : { rejectUnauthorized: false },
+    process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : {},
 };
--- apps/accounting-service/src/app/app.module.ts
+++ apps/accounting-service/src/app/app.module.ts
@@ -26,7 +26,10 @@ dotenv.config();
           password: configService.get('DB_PASSWORD', process.env.DB_PASSWORD),
           database: configService.get('ACC_DB_NAME', process.env.ACC_DB_NAME),
           entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-          // ssl: { rejectUnauthorized: false },
+          ssl:
+            process.env.NODE_ENV === 'production'
+              ? { rejectUnauthorized: true }
+              : {},
         } as TypeOrmModuleOptions),
     }),
     AccountsModule,
