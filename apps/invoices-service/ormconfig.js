d68be359e5da0e7caac5b7f024d523b31ece9042
zeeshooo <zeeshan@invyce.com>
2022-04-13 13:11:55 +0500

    changes

--- apps/invoices-service/ormconfig.js
+++ apps/invoices-service/ormconfig.js
@@ -3,11 +3,11 @@ require('dotenv').config({ path: __dirname + '/.env' });
 
 module.exports = {
   type: 'postgres',
-  host: process.env.INV_DB_HOST,
-  port: parseInt(process.env.INV_DB_PORT, 10),
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
   username: process.env.INV_DB_USER,
-  password: process.env.INV_DB_PASSWORD,
-  database: process.env.INV_DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
   migrationsTableName: 'custom_migration_table',
   migrations: ['./apps/invoices-service/migrations/**/*.ts'],
