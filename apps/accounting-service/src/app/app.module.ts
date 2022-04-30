c5503173aaf016364bf253911d4f8de19bdefcb9
zeeshooo <zeeshan@invyce.com>
2022-04-30 17:07:08 +0500

    fixes

--- apps/accounting-service/src/app/app.module.ts
+++ apps/accounting-service/src/app/app.module.ts
@@ -27,7 +27,7 @@ dotenv.config();
           password: configService.get('DB_PASSWORD', process.env.DB_PASSWORD),
           database: configService.get('ACC_DB_NAME', process.env.ACC_DB_NAME),
           entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-          // ssl: { rejectUnauthorized: false },
+          ssl: { rejectUnauthorized: false },
         } as TypeOrmModuleOptions),
     }),
     AccountsModule,
