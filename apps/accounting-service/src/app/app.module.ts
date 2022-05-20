8a5c15463f4c16ac69509b1438f1d19ec529a235
zeeshooo <zeeshan@invyce.com>
2022-05-20 12:54:28 +0500

    changes

--- apps/accounting-service/src/app/app.module.ts
+++ apps/accounting-service/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { getMetadataArgsStorage } from 'typeorm';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
@@ -11,6 +13,12 @@ import { BankModule } from './bank/bank.module';
 import { ReportModule } from './report/report.module';
 
 dotenv.config();
+
+let content;
+if (process.env['NODE' + '_ENV'] === 'production') {
+  // read from a file
+  content = fs.readFileSync(path.join(__dirname, '/vault/secrets/db-creds'));
+}
 
 @Module({
   imports: [
@@ -21,11 +29,26 @@ dotenv.config();
       useFactory: async (configService: ConfigService) =>
         ({
           type: 'postgres',
-          host: configService.get('DB_HOST', process.env.DB_HOST),
-          port: configService.get<unknown>('DB_PORT', process.env.DB_PORT),
-          username: configService.get('DB_USER', process.env.DB_USER),
-          password: configService.get('DB_PASSWORD', process.env.DB_PASSWORD),
-          database: configService.get('ACC_DB_NAME', process.env.ACC_DB_NAME),
+          host: configService.get(
+            'DB_HOST',
+            process.env.DB_HOST || content.DB_HOST
+          ),
+          port: configService.get<unknown>(
+            'DB_PORT',
+            process.env.DB_PORT || content.DB_PORT
+          ),
+          username: configService.get(
+            'DB_USER',
+            process.env.DB_USER || content.DB_USER
+          ),
+          password: configService.get(
+            'DB_PASSWORD',
+            process.env.DB_PASSWORD || content.DB_PASSWORD
+          ),
+          database: configService.get(
+            'ACC_DB_NAME',
+            process.env.ACC_DB_NAME || content.ACC_DB_NAME
+          ),
           entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
           ssl: { rejectUnauthorized: false },
         } as TypeOrmModuleOptions),
