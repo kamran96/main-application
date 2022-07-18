c37c54103af5bf0ea003439a10c7382fe7e9374c
zeeshooo <zeeshan@invyce.com>
2022-07-18 12:49:20 +0500

    changes in attachments-service

---
+++ apps/attachments-service/src/attachment/attachment.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Attachment, AttachmentSchema } from '../schemas/attachment.schema';
+
+import { AppController } from './attachment.controller';
+import { AttachmentService } from './attachment.service';
+import { Authenticate } from '@invyce/auth-middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      {
+        name: Attachment.name,
+        schema: AttachmentSchema,
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AttachmentService],
+})
+export class AttachmentModule {
+  configure(route) {
+    route.apply(Authenticate).forRoutes(AppController);
+  }
+}
