9e73a24c3761f9d9f607493b86276ded402f8d85
zeeshooo <zeeshan@invyce.com>
2022-07-26 16:00:15 +0500

    changes

--- apps/users-service/src/app/app.module.ts
+++ apps/users-service/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import * as fs from 'fs';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,9 +10,46 @@ import { OrganizationModule } from './organization/organization.module';
 import { RbacModule } from './rbac/rbac.module';
 import { UserModule } from './user/user.module';
 
+console.log(process.env['NODE' + '_ENV'], 'env');
+
+let MONGO_URI;
+if (process.env['NODE' + '_ENV'] !== 'development') {
+  // read from a file
+
+  const pathToStaticContent = path.join(
+    __dirname,
+    '../../../vault/secrets/creds'
+  );
+
+  const staticContentFromVault = fs.readFileSync(
+    path.join(pathToStaticContent),
+    {
+      encoding: 'utf8',
+    }
+  );
+
+  // static Content
+  const staticContentWithoutLineBreaks = staticContentFromVault.replace(
+    /[\r\n]/gm,
+    ''
+  );
+  const staticContentObj = `{${staticContentWithoutLineBreaks}}`;
+  const staticContent = JSON.parse(staticContentObj);
+
+  if (process.env['NODE' + '_ENV'] === 'staging') {
+    console.log('staging');
+    MONGO_URI = staticContent.STAGE_MONGO_URI;
+  } else if (process.env['NODE' + '_ENV'] === 'production') {
+    console.log('production');
+    MONGO_URI = staticContent.MONGO_URI;
+  }
+}
+
+console.log(MONGO_URI, 'uri');
+
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI || MONGO_URI),
     AuthModule,
     UserModule,
     RbacModule,
