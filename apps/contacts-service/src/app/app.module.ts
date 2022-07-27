b2544afe0c8d05e964627bfb09b39594ab031984
zeeshooo <zeeshan@invyce.com>
2022-07-27 10:49:25 +0500

    change

--- apps/contacts-service/src/app/app.module.ts
+++ apps/contacts-service/src/app/app.module.ts
@@ -1,12 +1,43 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import * as fs from 'fs';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 
+let MONGO_URI;
+
+if (process.env['NODE' + '_ENV'] === 'production') {
+  // read from a file
+
+  const pathToStaticContent = path.join(
+    __dirname,
+    '../../../vault/secrets/creds'
+  );
+
+  const staticContentFromVault = fs.readFileSync(
+    path.join(pathToStaticContent),
+    {
+      encoding: 'utf8',
+    }
+  );
+
+  // static Content
+  const staticContentWithoutLineBreaks = staticContentFromVault.replace(
+    /[\r\n]/gm,
+    ''
+  );
+  const staticContentObj = `{${staticContentWithoutLineBreaks}}`;
+  const staticContent = JSON.parse(staticContentObj);
+  MONGO_URI = staticContent.MONGO_URI;
+}
+
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), ContactModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || MONGO_URI),
+    ContactModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
