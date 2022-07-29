f96ca981b34d5b697768d635d79a1c8eca4d5689
zeeshooo <zeeshan@invyce.com>
2022-07-29 14:58:15 +0500

    changes

--- apps/users-service/src/app/auth/auth.strategy.ts
+++ apps/users-service/src/app/auth/auth.strategy.ts
@@ -32,6 +32,7 @@ export class AuthStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: IBaseUser): Promise<IUser> {
     try {
+      console.log(payload, 'pay');
       const newData = { ...data, user: payload };
       const user: IUserAccessControlResponse =
         await this.authService.AccessControll(newData as IRequest);
